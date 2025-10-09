4894ee01c4aa204847a6dd2055ed5137389f0c19
Vincent Wölfer <vincent.woelfer@gmail.com>
2025-10-09 14:53:39 +0200

    Update HexLog from dwarf game

--- src/utils/HexLog.gd
+++ src/utils/HexLog.gd
@@ -7,23 +7,28 @@ class_name HexLog
 const BANNER_WIDTH: int = 64
 const BANNER_CHAR: String = "="
 
+## Prints a one-line banner
 static func print_only_banner() -> void:
 	print(BANNER_CHAR.repeat(BANNER_WIDTH))
 
+## Prints text sourounded by a one-line banner
 static func print_banner_with_text(string: String) -> void:
 	# Souround string with spaces
 	string = " " + string + " "
 
-	print(center_text(string, BANNER_WIDTH, BANNER_CHAR))
+	print(_center_text(string, BANNER_WIDTH, BANNER_CHAR))
 
+## Prints text sourounded by a multi-line banner
 static func print_multiline_banner_with_text(string: String) -> void:
 	# Souround string with spaces
 	string = " " + string + " "
 
 	var banner_line: String = BANNER_CHAR.repeat(BANNER_WIDTH)
-	print(banner_line, "\n", center_text(string, BANNER_WIDTH, BANNER_CHAR), "\n", banner_line)
+	print(banner_line, "\n", _center_text(string, BANNER_WIDTH, BANNER_CHAR), "\n", banner_line)
 
-static func center_text(text: String, width: int, filler: String) -> String:
+
+## Helper function: Centers text within a given width using a given filler character
+static func _center_text(text: String, width: int, filler: String) -> String:
 	var pad_size_total: int = max(0, (width - text.length()))
 
 	var pad_size_left: int
@@ -37,54 +42,3 @@ static func center_text(text: String, width: int, filler: String) -> String:
 		pad_size_right = pad_size_left + 1
 
 	return filler.repeat(pad_size_left) + text + filler.repeat(pad_size_right)
-
-
-
-##################################################################
-# Helper Functions for displaying all key mappings. Old, maybe reuse
-###################################################################
-# func get_input_mapping() -> Dictionary[String, String]:
-# 	var mapping: Dictionary[String, String] = {}
-# 	for action in InputMap.get_actions():
-# 		if not (action.begins_with("ui_") or action.begins_with("cam_") or action.begins_with("spatial_")):
-# 			mapping[action] = get_keys_for_action(action)
-# 	return mapping
-
-
-# func get_keys_for_action(action: String) -> String:
-# 	var key_list := []
-# 	var events := InputMap.action_get_events(action)
-# 	for event in events:
-# 		if event is InputEventKey:
-# 			# Add the physical key code to the list
-# 			var key_string: String = (event as InputEventKey).as_text_physical_keycode()
-# 			key_list.append(key_string)
-# 		elif event is InputEventJoypadButton:
-# 			var key_string: String = (event as InputEventJoypadButton).as_text()
-# 			key_list.append(key_string)
-# 	return ", ".join(key_list)
-
-
-# func pretty_print_actions(actions_dict: Dictionary[String, String]) -> void:
-# 	var max_action_length: int = 0
-# 	for action: String in actions_dict.keys():
-# 		max_action_length = max(max_action_length, action.length())
-
-# 	# Sort the dictionary by values (hotkeys) alphabetically
-# 	var sorted_hotkeys: Array[String] = actions_dict.values()
-# 	sorted_hotkeys.sort_custom(func(a: String, b: String) -> bool: return a < b)
-
-# 	HexLog.print_banner_with_text("Key Bindings")
-# 	for hotkey: String in sorted_hotkeys:
-# 		var action := get_key_by_value(actions_dict, hotkey)
-# 		var padding: String = " ".repeat(max_action_length - action.length())
-# 		print("- %s:%s    %s" % [action, padding, actions_dict[action]])
-# 	HexLog.print_only_banner()
-
-
-# func get_key_by_value(dict: Dictionary[String, String], value: String) -> String:
-# 	for key: String in dict:
-# 		if dict[key] == value:
-# 			return key
-# 	# Return an empty string if no matching value is found
-# 	return ""
